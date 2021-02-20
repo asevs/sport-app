1776205a366038c3e6edd09179388632e14a9edb
Lukasz <gorcek45@gmail.com>
2021-02-20 14:58:36 +0100

    Init project

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+// .eslintrc.js
+module.exports = {
+  extends: ['airbnb-typescript-prettier'],
+  rules: {
+    'no-unused-vars': ['error', { vars: 'all', args: 'after-used' }],
+  },
+  env: {},
+};
---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import './App.css';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: inline;
+`;
+
+const App: React.FunctionComponent = () => {
+  return (
+    <>
+      <Wrapper>
+        <Wrapper>dsadsa</Wrapper>
+        <Wrapper>dsadsa</Wrapper>
+        <Wrapper>dsadsa</Wrapper>
+      </Wrapper>
+    </>
+  );
+};
+
+export default App;
